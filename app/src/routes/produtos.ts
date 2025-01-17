89f34e372ab73b12feaa4e618b0185b43563896b
MarkHiarley <hiarleycostamarley@gmail.com>
2025-01-17 12:56:55 -0300

    route put

--- app/src/routes/produtos.ts
+++ app/src/routes/produtos.ts
@@ -15,8 +15,8 @@ router.post('/', async (req: Request, res: Response) => {
         res.status(201).json(newProduto.rows[0]);
         console.log('Produto adicionado com sucesso' + newProduto.rows[0]);
 
-    } catch (error) {
-        console.log(error);
+    } catch (erro : any) {
+        console.log(erro);
         res.status(500).send('Erro ao adicionar produto');
     }
 });
@@ -28,7 +28,7 @@ router.get('/', async (req: Request, res: Response) => {
             return res.status(200).json('Não há produtos para buscar')
         }
         res.status(200).json(Produtos.rows)
-    }catch(error){
+    }catch(error: any){
         res.status(500).json("erro ao buscar" + error)
     }
 })
@@ -41,10 +41,28 @@ router.delete('/:id', async (req: Request, res: Response) => {
     try {
         const produtoDelete = await pool.query('DELETE FROM produtos WHERE id = $1',[id])
         res.status(200).json(`produto com id: ${id} deletado com sucesso` + produtoDelete)
-    }catch(error){
+    }catch(error: any){
         res.status(500).json("erro ao deletar" + error)
     }
 })
 
-
+router.put('/:id' , async (req: Request, res:Response)=> {
+    const { nome, categoria, foto } = req.body
+    const restaurante_id = parseInt(req.body.restaurante_id)
+    const id = parseInt(req.params.id)
+    const preco = parseFloat(req.body.preco)
+    try{
+        if(!nome || !categoria || !restaurante_id || !preco || !foto){
+            return res.status(404).json("informe todo os valores")
+        }
+        const produtoAtualizado = await pool.query(
+            'UPDATE produtos SET restaurante_id= $1, nome = $2, foto = $3, preco = $4, categoria = $5 WHERE id = $6',
+            [restaurante_id, nome, foto, preco, categoria, id ]
+        );
+        res.status(201).json(`produto com o id:${id} atualizado com sucesso` + produtoAtualizado)
+    }catch(erro:any){
+        res.status(500).json(erro+"erro ao conectar o banco de dados ou api")
+        console.log(erro)
+    }
+})
 export default router;

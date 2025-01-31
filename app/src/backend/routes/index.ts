3409f8bf9378a786f848ee904e4e01bfe3209eec
MarkHiarley <hiarleycostamarley@gmail.com>
2025-01-31 15:56:43 -0300

    backend pasta criada

--- app/src/backend/routes/index.ts
+++ app/src/backend/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import restaurantesRouter from './restaurantes';
 import produtosRouter from './produtos';
 import promocoesRouter from './promocoes';
+import horariosRouter from './horarios';
 
 const router = Router();
 
 router.use('/restaurantes', restaurantesRouter);
 router.use('/produtos', produtosRouter);
 router.use('/promocoes', promocoesRouter);
+router.use('/horarios', horariosRouter);
 
 
 export default router;

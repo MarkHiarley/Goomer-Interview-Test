97e461f35750be02ab57817a26b2ea353173e930
MarkHiarley <hiarleycostamarley@gmail.com>
2025-02-01 00:04:28 -0300

    routes finished

--- app/src/backend/routes/horarios.ts
+++ app/src/backend/routes/horarios.ts
@@ -4,10 +4,10 @@ import pool from '../database/pgconnect';
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    try{
-    const horarios = pool.query('SELECT * FROM horarios_restaurante')
-    res.status(200).json((await horarios).rows[0])
-    }catch(error){
+    try {
+        const horarios = pool.query('SELECT * FROM horarios_restaurante')
+        res.status(200).json((await horarios).rows[0])
+    } catch (error) {
         res.status(500).json("erro ao buscar horarios" + error)
     }
 })
@@ -34,4 +34,46 @@ router.post('/', async (req: Request, res: Response) => {
     }
 });
 
+router.delete('/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        if (!id) {
+            return res.status(404).json('Informe um id valido')
+        }
+        await pool.query(`DELETE FROM horarios_restaurante WHERE id = ${id}`)
+        res.status(200).json(`Produto com id ${parseInt(id)} DELETADO`)
+    } catch (error) {
+        res.status(500).json('erro ao deletar o produto')
+        console.log(error)
+    }
+})
+
+router.patch('/:id', async (req: Request, res: Response) => {
+    try {
+        const { dia_da_semana, hora_inicio, hora_fim, restaurante_id } = req.body
+        const id = req.params.id
+        const validRestauranteId = restaurante_id !== null && !isNaN(restaurante_id) ? restaurante_id : null
+        let query = 'UPDATE horarios_restaurante SET '
+        const values: any[] = [];
+        const fields: string[] = [];
+        const data = { dia_da_semana, hora_inicio, hora_fim, restaurante_id: validRestauranteId, }
+
+        Object.entries(data).forEach(([key, value]) => {
+            if (value !== null && value !== undefined) {
+                fields.push(`${key} = $${fields.length + 1}`)
+                values.push(value)
+            }
+        })
+        query += fields.join(', ')
+        query += ` WHERE id = $${fields.length + 1}`
+        values.push(id)
+
+        await pool.query(query, values)
+        res.status(201).json(`Horario com o id:${id} atualizado com sucesso`)
+    } catch (error) {
+        res.status(500).json(error + "erro ao conectar o banco de dados ou api")
+        console.log(error)
+    }
+
+})
 export default router;

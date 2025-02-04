151e6bd849a40b9f28e25cc2722f21f37d20ba0c
MarkHiarley <hiarleycostamarley@gmail.com>
2025-02-04 12:02:39 -0300

    login route

--- app/backend/routes/index.ts
+++ app/backend/routes/index.ts
@@ -3,13 +3,16 @@ import restaurantesRouter from './restaurantes';
 import produtosRouter from './produtos';
 import promocoesRouter from './promocoes';
 import horariosRouter from './horarios';
+import loginAPI from './login/login'
 
 const router = Router();
+
 
 router.use('/restaurantes', restaurantesRouter);
 router.use('/produtos', produtosRouter);
 router.use('/promocoes', promocoesRouter);
 router.use('/horarios', horariosRouter);
+router.use('/login', loginAPI);
 
 
 export default router;

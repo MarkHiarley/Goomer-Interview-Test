3e28485de89705ff8d1682e0af04fcbace3f64f4
MarkHiarley <hiarleycostamarley@gmail.com>
2025-03-13 11:42:18 -0300

    ajuste na auth

--- app/backend/token/verify.AccessToken.ts
+++ app/backend/token/verify.AccessToken.ts
@@ -1,28 +1,34 @@
-import jwt from 'jsonwebtoken'
-const secretKey = process.env.SECRET_KEY
+import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
+const secretKey = process.env.SECRET_KEY;
+declare module 'express-serve-static-core' {
+    interface Request {
+        user?: any; 
+    }
+}
 
 export async function verifyAccessToken(req: Request, res: Response, next: NextFunction) {
     try {
         if (!secretKey) {
             throw new Error('SECRET_KEY não está definida nas variáveis de ambiente');
         }
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
+        const authCookies = req.cookies.cookieTest;
+        if (!authCookies) {
             return res.status(401).json({ message: 'Token não fornecido' });
         }
 
-        const token = authHeader.replace("Bearer ", '');
+        const token = authCookies.replace("Bearer ", '');
         if (!token) {
             return res.status(401).json({ message: 'Token não fornecido' });
         }
-        const decoded = jwt.verify(token, secretKey)
-        next()
+        const decoded = jwt.verify(token, secretKey);
+        req.user = decoded;
+
+        next();
     } catch (error) {
         return res.status(401).json({ message: 'Token inválido ou expirado' });
     }
 }
-
 
 export default verifyAccessToken;
